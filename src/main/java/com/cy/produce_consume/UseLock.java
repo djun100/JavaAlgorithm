ba6d82f9a3d0a25a16c21818f4fe85b853cd0420
wangxuechao <djun100@qq.com>
2020-11-12 17:25:44 +0800

    生产消费者

--- src/main/java/com/cy/produce_consume/UseLock.java
+++ src/main/java/com/cy/produce_consume/UseLock.java
@@ -1,81 +1,79 @@
 package com.cy.produce_consume;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedList;
 import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class UseLock {
-    List<Integer> list = new ArrayList<>();
+    ReentrantLock lock = new ReentrantLock();
+    Condition full = lock.newCondition();
+    Condition empty = lock.newCondition();
+    LinkedList<Integer> list = new LinkedList<>();
     int max = 3;
-    Lock lock =new ReentrantLock();
-    Condition notFullCondition = lock.newCondition();
-    Condition notEmptyCondition = lock.newCondition();
-
-    class Produce implements Runnable{
-
-        @Override
-        public void run() {
-            while (true) {
-                try {
-                    lock.lock();
-                    System.out.println(Thread.currentThread()+"通过锁进入生产");
-                    if (list.size()<max){
-                        list.add(new Random().nextInt());
-                        System.out.println(Thread.currentThread()+"总量："+list.size());
-                        Thread.sleep(2000);
-                    }else {
-                        System.out.println(Thread.currentThread()+"生产已满，等待");
-                        notFullCondition.await();
-                    }
-                    System.out.println(Thread.currentThread()+"不空，通知消费");
-                    notEmptyCondition.signal();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } finally {
-                    System.out.println(Thread.currentThread()+"通过锁退出生产");
-                    lock.unlock();
-                }
-            }
-        }
-    }
-
-    class Consume implements Runnable{
-
-        @Override
-        public void run() {
-            while (true) {
-                try {
-                    lock.lock();
-                    System.out.println(Thread.currentThread()+"通过锁进入消费");
-                    if (list.size()>0){
-                        list.remove(list.size()-1);
-                        System.out.println(Thread.currentThread()+"总量："+list.size());
-                        Thread.sleep(2000);
-                    }else {
-                        System.out.println(Thread.currentThread()+"已消费空，等待");
-                        notEmptyCondition.await();
-                    }
-                    System.out.println(Thread.currentThread()+"不满，通知生产");
-                    notFullCondition.signal();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } finally {
-                    System.out.println(Thread.currentThread()+"通过锁退出消费");
-                    lock.unlock();
-                }
-            }
-
-        }
-    }
 
     public static void main(String[] args) {
-        UseLock useLock = new UseLock();
-        new Thread(useLock.new Produce()).start();
-        new Thread(useLock.new Produce()).start();
-        new Thread(useLock.new Consume()).start();
-        new Thread(useLock.new Consume()).start();
+        UseLock useLock =new UseLock();
+        ExecutorService service = Executors.newFixedThreadPool(4);
+        for (int i = 0; i < 2; i++) {
+            service.submit(useLock.new Productor());
+        }
+        for (int i = 0; i < 2; i++) {
+            service.submit(useLock.new Consumer());
+        }
+    }
+    class Productor implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                lock.lock();
+                try {
+                    while (list.size() == max) {
+                        System.out.println("生产者" + Thread.currentThread().getName() + "  list以达到最大容量，进行wait");
+                        full.await();
+                        System.out.println("生产者" + Thread.currentThread().getName() + "  退出wait");
+                    }
+                    Random random = new Random();
+                    int i = random.nextInt();
+                    System.out.println("生产者" + Thread.currentThread().getName() + " 生产数据" + i);
+                    list.add(i);
+                    Thread.sleep(2000);
+                    empty.signalAll();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+        
+    }
+    class Consumer implements Runnable {
+
+        @Override
+        public void run() {
+            while (true) {
+                lock.lock();
+                try {
+                    while (list.isEmpty()) {
+                        System.out.println("消费者" + Thread.currentThread().getName() + "  list为空，进行wait");
+                        empty.await();
+                        System.out.println("消费者" + Thread.currentThread().getName() + "  退出wait");
+                    }
+                    Integer element = list.remove(0);
+                    System.out.println("消费者" + Thread.currentThread().getName() + "  消费数据：" + element);
+                    Thread.sleep(2000);
+                    full.signalAll();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        }
+        
     }
 }
